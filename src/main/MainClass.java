5a725d178797a892ee7b6c15923d3dcd90037c4f
ali <ali_lotfdar@yahoo.com>
2015-02-22 19:00:40 -0500

    third commit

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Helloo Ali");
+		System.out.println("Helloo Ali Jan");
 
 	}
 
