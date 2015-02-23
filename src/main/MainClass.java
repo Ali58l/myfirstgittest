9cda4b9b22a4771a840cbbf64e956b2cfcfaa21f
ali <ali_lotfdar@yahoo.com>
2015-02-22 19:15:52 -0500

    "ready for git repository online"

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -5,6 +5,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//https://github.com/Ali58l/myfirstgittest.git
+		
 		System.out.println("Helloo Ali Jan");
 		System.out.println("khoooobi??");
 
